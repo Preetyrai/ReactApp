c3ebcf6ac8f8b9dd224a848faca6fbd9977147c2
preety.drai <preety.drai@gmail.com>
2023-04-17 22:41:03 +0530

    changes in function file

--- functions/hello.js
+++ functions/hello.js
@@ -1,13 +1,13 @@
 //domain/.netlify/functions/hello
 
 const items = [
-    {id :1, name: "Preety"},
-    {id:2, name:"susan"}
-]
+  { id: 1, name: "Preety" },
+  { id: 2, name: "susan" },
+];
 
-exports.handler =  async function (event, context){
-    return {
-   statusCode:200,
-   body: 'hello world'
-    }
-}
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: "hello world",
+  };
+};
